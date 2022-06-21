234361113858b693498d5f92c064212301030dbb
pmita <panos.mita2@outlook.com>
2022-06-21 13:31:23 +0100

    fetching data from our json-server

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,17 @@
 import './Home.css';
+//HOOKS
+import { useFetch } from '../../hooks/useFetch';
 
 const Home = () => {
+    //STATE &&VARIABLES
+    const { data, isPending, error } = useFetch('http://localhost:3000/recipes');
     return(
-        <div>
-            Welcome to Home Page
+        <div className='home'>
+            {error && <p className='error'>{error}</p>}
+            {isPending && <p className='loading'>Loading...</p>}
+            {data && data.map(recipe => (
+                <h2 key={recipe.id}>{recipe.title}</h2>
+            ))}
         </div>
     );
 }
